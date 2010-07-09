c028e8c9957738a9376428584d50994cbe77be46
J. Brisbin <jon@jbrisbin.com>
2010-07-09 14:33:50 -0500

    Tweaked tests, fixed bug in pom.xml

--- async-cache/src/test/java/com/jbrisbin/vcloud/cache/test/AsyncCacheProviderTest.java
+++ async-cache/src/test/java/com/jbrisbin/vcloud/cache/test/AsyncCacheProviderTest.java
@@ -126,4 +126,24 @@ public class AsyncCacheProviderTest {
     return obj;
   }
 
+  static class TestObject implements Serializable {
+
+    private String data;
+
+    public String getData() {
+      return data;
+    }
+
+    public void setData( String data ) {
+      this.data = data;
+    }
+
+    @Override
+    public String toString() {
+      String s = super.toString();
+      return s + ",data=" + data.substring( 0, 16 ) + "...";
+    }
+
+  }
+
 }
